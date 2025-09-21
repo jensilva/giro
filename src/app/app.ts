603af989c894fbea3ebe62e7546747c0d021d06f
Jennfer Silva <jenny.ms@outlook.com.br>
2025-09-21 20:40:26 +0100

    página de login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login} from './login/login';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideRouter } from '@angular/router';
+import routeConfig from "./app/app.routes"
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(appConfig, App,
+{
+  providers: [
+    provideProtractorTestingSupport(),
+    provideRouter(routeConfig)
+  ]
+})
   .catch((err) => console.error(err));
